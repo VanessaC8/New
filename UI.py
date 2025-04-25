d1526a1d86f42774347c886124f9cc09c7206ba5
VanessaC8 <vcastro5656@gmail.com>
2025-04-25 06:24:29 +0000

    added

--- UI.py
+++ UI.py
@@ -1,4 +1,10 @@
 import tkinter as tk
 
 window = tk.Tk()
-
+window.title("Simple GUI")
+window.geometry("400x300")
+def on_button_click():
+    print("Button clicked!")
+button = tk.Button(window, text="Click Me", command=on_button_click)
+button.pack(pady=20)
+label = tk.Label(window, text="Hello, Tkinter!")
