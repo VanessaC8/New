cdb0a10b998a169babbaa4f0576389376ef51156
VanessaC8 <vcastro5656@gmail.com>
2025-04-25 06:26:08 +0000

    added

--- UI.py
+++ UI.py
@@ -8,3 +8,6 @@ def on_button_click():
 button = tk.Button(window, text="Click Me", command=on_button_click)
 button.pack(pady=20)
 label = tk.Label(window, text="Hello, Tkinter!")
+
+label.pack(pady=10)
+window.mainloop()
