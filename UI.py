5f8e45d63803d7739901eecdb2c5dc7b443d7d07
VanessaC8 <vcastro5656@gmail.com>
2025-04-25 06:35:46 +0000

    change

--- UI.py
+++ UI.py
@@ -1,13 +1,9 @@
-#import tkinter as tk
+import tkinter as tk
 
-#window = tk.Tk()
-#window.title("Simple GUI")
-#window.geometry("400x300")
-#def on_button_click():
-#    print("Button clicked!")
-#button = tk.Button(window, text="Click Me", command=on_button_click)
-#button.pack(pady=20)
-#label = tk.Label(window, text="Hello, Tkinter!")
+def on_button_click():
+    print("Button clicked!")
 
-#label.pack(pady=10)
-#window.mainloop()
+window = tk.Tk()
+button = tk.Button(window, text="Click Me", command=on_button_click)
+button.pack()
+window.mainloop()
